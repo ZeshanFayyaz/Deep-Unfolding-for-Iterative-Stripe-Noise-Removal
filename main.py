c370f626a41b239b26f0e96b4864380ae23fe743
DanielPlatnick <daniel.platnick@gmail.com>
2021-07-28 14:29:20 -0400

    Further modularized code

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
-
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
 import cv2
 import os
 import numpy as np
 import random
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, GRU, Bidirectional, TimeDistributed
-import matplotlib.pyplot as plt
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+
+
 
 def main():
     # Degrade Function which takes the clean image as input and returns the noisy image and the noise itself. Same dimensionality, I.e. (256,256)
@@ -26,7 +26,6 @@ def main():
 
         image_G = image + G_noise
         return image_G, G_noise
-
 
     # The Training + Validation Data is all the images in the datadir directory, resized to IMG_SIZE, IMG_SIZE
     def create_training_data():
@@ -47,7 +46,6 @@ def main():
                 pass
         return training_validation_data, training_data, validation_data
 
-
     def create_testing_data():
         testing_data = []
         for img in os.listdir(testdir):
@@ -57,10 +55,9 @@ def main():
                 new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
                 testing_data.append([new_array])
             # Pass exception in case we are unable to read an image due to corruption, ignore that image
-            except Exception as e:
+            except Exception:
                 pass
         return testing_data
-
 
     def degrade_test_data():
         model_testing_data = []
@@ -74,6 +71,46 @@ def main():
         clean_testing_data = np.array(clean_testing_data)
         model_testing_data = np.array(model_testing_data)
         return clean_testing_data, model_testing_data
+
+    def train_model():
+        inputs = Input(shape=(256, 256))
+        # Transpose the input shape replacing rows with columns and columns with rows
+        inputs_t = tf.transpose(inputs, perm=[0, 2, 1])
+        # We perform this manually for ease of visualization, however can be done in a for loop
+        # MODEL 3
+        # Layer 1
+        # Return sequences is TRUE as we want an output for every timestep, and not a "many-to-one" output
+        # Merge_mode is set to AVERAGE - in order to maintain dimensionality (256,256) [default is CONCAT]
+        output_1 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(inputs_t)
+        # The input of layer 2 is the output of layer 0 [transpose] SUBTRACT the output of layer 1
+        input_2 = tf.keras.layers.Subtract()([inputs_t, output_1])
+        output_2 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(input_2)
+        input_3 = keras.layers.Subtract()([input_2, output_2])
+        output_3 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(input_3)
+        input_4 = keras.layers.Subtract()([input_3, output_3])
+        output_4 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(input_4)
+        input_5 = keras.layers.Subtract()([input_4, output_4])
+        output_5 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(input_5)
+        input_6 = keras.layers.Subtract()([input_5, output_5])
+        output_6 = Bidirectional(GRU(256, return_sequences=True), merge_mode="ave")(input_6)
+        # Perform TimeDistributed Operation to final output of GRU
+        output_GRU = TimeDistributed(Dense(256))(output_6)
+        # Transpose the image once again, giving us original dimensionality
+        output_GRU = tf.transpose(output_GRU, perm=[0, 2, 1])
+        # We aim for the real_output to be as close as possible to 'z' -> the Clean Image
+        # The clean image is the dirty image [Input] SUBTRACT the noise [Output of the Network (output_GRU)]
+        real_output = tf.keras.layers.Subtract()([inputs, output_GRU])
+        model = Model(inputs=inputs, outputs=real_output)
+        model.summary()
+        print("Model Compiled")
+        opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
+        model.compile(
+            optimizer=opt,
+            loss='mean_squared_error',
+            metrics=['mse']
+        )
+
+        return model
 
     # The paths to the image locations
     # datadir refers to directory that contains the clean images for TRAINING
@@ -108,7 +145,7 @@ def main():
     # We can perform the same operation on Testing Data
     print("Creating Testing Data...")
 
-    # create testing data
+    # Create testing data
     testing_data = create_testing_data()
     testing_data = np.array(testing_data)
     print("Testing Data Length: " + str(len(testing_data)))
@@ -129,58 +166,7 @@ def main():
     X_debug = []
     z_debug = []
 
-
-    inputs = Input(shape=(256, 256))
-    # Transpose the input shape replacing rows with columns and columns with rows
-    inputs_t = tf.transpose(inputs, perm=[0, 2, 1])
-
-    # We perform this manually for ease of visualization, however can be done in a for loop
-    # MODEL 3
-    # Layer 1
-    # Return sequences is TRUE as we want an output for every timestep, and not a "many-to-one" output
-    # Merge_mode is set to AVERAGE - in order to maintain dimensionality (256,256) [default is CONCAT]
-    output_1 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(inputs_t)
-    # The input of layer 2 is the output of layer 0 [transpose] SUBTRACT the output of layer 1
-    input_2 = tf.keras.layers.Subtract()([inputs_t, output_1])
-    # Layer 2
-    output_2 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(input_2)
-    # The input of layer 3 is the input of layer 2 SUBTRACT the output of layer 2
-    input_3 = keras.layers.Subtract()([input_2, output_2])
-    # layer 3
-    output_3 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(input_3)
-    # The input of layer 4 is the input of layer 3 SUBTRACT the output of layer 3
-    input_4 = keras.layers.Subtract()([input_3, output_3])
-    # Layer 4
-    output_4 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(input_4)
-    # The input of layer 5 is the input of layer 4 SUBTRACT the output of layer 4
-    input_5 = keras.layers.Subtract()([input_4, output_4])
-    # Layer 5
-    output_5 = (Bidirectional(GRU(256, return_sequences=True), merge_mode="ave"))(input_5)
-    # The input of layer 6 is the input of layer 5 SUBTRACT the output of layer 5
-    input_6 = keras.layers.Subtract()([input_5, output_5])
-    # Layer 6
-    output_6 = Bidirectional(GRU(256, return_sequences=True), merge_mode="ave")(input_6)
-    # Perform TimeDistributed Operation to final output of GRU
-    # Performs operation on each temporal slice of output
-    output_GRU = TimeDistributed(Dense(256))(output_6)
-
-    # Transpose the image once again, giving us original dimensionality
-    output_GRU = tf.transpose(output_GRU, perm=[0, 2, 1])
-    # We aim for the real_output to be as close as possible to 'z' -> the Clean Image
-    # The clean image is the dirty image [Input] SUBTRACT the noise [Output of the Network (output_GRU)]
-    real_output = tf.keras.layers.Subtract()([inputs, output_GRU])
-
-    model = Model(inputs=inputs, outputs=real_output)
-    model.summary()
-    print("Model Compiled")
-
-    opt = tf.keras.optimizers.Adam(lr=1e-3, decay=1e-5)
-
-    model.compile(
-        optimizer=opt,
-        loss='mean_squared_error',
-        metrics=['mse']
-    )
+    model = train_model()
     for i in range(num_epochs):
         X = []
         z = []
@@ -212,7 +198,7 @@ def main():
         X_debug = X[0:2]
         z_debug = z[0:2]
 
-        # We can reshape the arrays as well as convert them to numpy arrays
+        # Reshape arrays and convert to numpy arrays
         #X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE)
         #X_validation = np.array(X_validation).reshape(-1, IMG_SIZE, IMG_SIZE)
         #X_debug = np.array(X_debug).reshape(-1, IMG_SIZE, IMG_SIZE)
@@ -225,23 +211,15 @@ def main():
 
         print("Reshaping Arrays... Done")
 
-        # The input shape refers to the size of the images used
-        # CIFAR10 Image Shape: (32,32)
-        # Natural_Dataset Image Shape: (256,256)
-
-
-        # Then, train the model with fit()
-        # We specify the batch size
-        # Epochs = 1 due to it being in a for loop
-        history = model.fit(X, z,
+        # Calculate loss
+        loss_metrics = model.fit(X, z,
                             batch_size=100,
                             epochs=1,
-                            # We manually set the validation data as defined at the top
                             validation_data=(X_validation, z_validation)
                             )
-        # We append the metrics to their respective lists
-        loss_train.append(history.history['loss'])
-        loss_val.append(history.history['val_loss'])
+        # Append metrics to their respective lists
+        loss_train.append(loss_metrics.history['loss'])
+        loss_val.append(loss_metrics.history['val_loss'])
         print("Done Epoch: " + str(i + 1))
 
     # Once training is complete, plot the Training Loss
@@ -319,7 +297,6 @@ def main():
 
     print("Train Image Sample...")
 
-
     fig2, (ax1t, ax2t, ax3t) = plt.subplots(1, 3, sharey=True, figsize=(11, 11))
     # Plot 1: Clean Testing Image
     ax1t.imshow(z_debug[0], cmap="gray")
@@ -339,7 +316,7 @@ def main():
     SSIM_train = structural_similarity(z[0], X[0])
     print("SSIM of Degraded Training Image in Reference to Ground Truth: " + str(SSIM_train))
     SSIM2_train = structural_similarity(z[0], output_train[0])
-    print("SSIM of Predicted Clean Image in Referen ce to Ground Truth: " + str(SSIM2_train))
+    print("SSIM of Predicted Clean Image in Reference to Ground Truth: " + str(SSIM2_train))
     print("Difference: " + str(SSIM2 - SSIM_train))
 
     PSNR_train = peak_signal_noise_ratio(z[0], X[0])
