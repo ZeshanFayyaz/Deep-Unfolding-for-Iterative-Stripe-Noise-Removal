55d0333e81d928e1bae73ea28c497ae97657ccdd
Zeshan Fayyaz <43391249+ZeshanFayyaz@users.noreply.github.com>
2021-07-28 23:48:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, GRU, Bidirectional, TimeDistributed
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 
+# If using the expanded 256x256 Dataset, set image_size = 256
+# If using the CIFAR Dataset, set image_size = 32
 image_size = 256
 num_epochs = 100
 datadir = "/home/zeshanfayyaz/LIA/Local_Images/Train/"
 testdir = "/home/zeshanfayyaz/LIA/Local_Images/Test/"
-test_output = "/home/zeshanfayyaz/LIA/test_output/"
+test_output = "/home/zeshanfayyaz/LIA/test_output/" # Where we want to save our test images and metrics
 
 def main():
+    
     # Degrade Function which takes the clean image as input and returns the noisy image and the noise itself.
     # Same dimensionality, I.e. (256,256)
     def degrade(image, noise_val):
@@ -34,50 +37,57 @@ def main():
         image_g = image + g_noise
         return image_g, g_noise
 
-    # The Training + Validation Data is all the images in the datadir directory, resized to image_size, image_size
+    
+    # The Training + Validation Data are all the images in the datadir directory, resized to image_size, image_size
+    # We shuffle the Data 
+    # Pass exception in case we are unable to read an image due to corruption - ignore that image
     def create_training_data():
         training_validation_data = []
         for img in os.listdir(datadir):
             try:
                 img_array = cv2.imread(os.path.join(datadir, img), cv2.IMREAD_GRAYSCALE)
-                # Resizing the Images to image_size,image_size
                 new_array = cv2.resize(img_array, (image_size, image_size))
                 training_validation_data.append([new_array, 0])
-                # Shuffling the data
                 random.shuffle(training_data)
-            # Pass exception in case we are unable to read an image due to corruption, ignore that image
             except Exception as e:
                 pass
         return training_validation_data
-
+    
+    
+    # The test images are within the testdir, same dimensionality as the train images (256,256) or (32,32)
+    # Pass exception in case we are unable to read an image due to corruption - ignore that image 
+    # We may also print the length of testing data to confirm correct file path 
     def create_testing_data():
         testing_data = []
         for img in os.listdir(testdir):
             try:
                 img_array = cv2.imread(os.path.join(testdir, img), cv2.IMREAD_GRAYSCALE)
-                # Resize the images in testdir to image_size, image_size
                 new_array = cv2.resize(img_array, (image_size, image_size))
                 testing_data.append([new_array])
-            # Pass exception in case we are unable to read an image due to corruption, ignore that image
             except Exception:
                 pass
         print("Testing Data Length: " + str(len(testing_data)))
         testing_data = np.array(testing_data)
         return testing_data
-
+    
+    
+    # We preprocess the testing_data created above and add a stripe noise with guassian value 25
+    # Normalize all images 
+    # We return lists containing the degraded test images, and clean test images, with equal lengths of testing_data
     def degrade_test_data():
         model_testing_data = []
         clean_testing_data = []
         for images in testing_data:
-            # We degrade the Test images with a value of 25
             model_image = degrade(images[0], 25)[0]
-            # We normalize all pixel values (max 255.0)
             model_testing_data.append(model_image / 255.0)
             clean_testing_data.append(images.squeeze() / 255.0)
         clean_testing_data = np.array(clean_testing_data)
         model_testing_data = np.array(model_testing_data)
         return clean_testing_data, model_testing_data
-
+    
+    
+    # We have previously read all images from datadir as training + validation images, here we perform the split
+    # Default value for split is 85% training and 15% validation
     def training_validation_split(training_validation, split=0.85):
         print("Using " + str(split * 100) + "% " + "Train and " + str(100 - (split * 100)) + "% " "Validation")
         print("Total Training + Validation Length: " + str(len(training_validation)))
@@ -87,7 +97,10 @@ def main():
         print("Training Data Length: " + str(len(training_data)))
         print("Validation Data Length: " + str(len(validation_data)))
         return training_data, validation_data
-
+    
+    
+    # Call this function when we want to inspect 1 image as: degraded, ground truth, and predicted
+    # "type" argument refers to setting the title of the subplot as "Testing Image" or "Training Image"
     def create_subplots(degraded, ground_truth, predicted, type):
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True, figsize=(11, 11))
         # Plot 1: Degraded Image
@@ -107,9 +120,15 @@ def main():
             fig.suptitle("Training Image")
         return fig
 
+    
     def average(lst):
         return sum(lst) / len(lst)
 
+    
+    # Calculate the PSNR and SSIM metrics using sklearn built-in calculations
+    # All calculations are performed with respect to the ground_truth image 
+    # For both SSIM and PSNR, we aim for a large positive difference
+    # If the difference is positive, our network is learning. Otherwise, the predicted image is worse quality than degraded
     def psnr_ssim_metrics(ground_truth, predicted, degraded):
         # PSNR
         psnr_degraded = peak_signal_noise_ratio(ground_truth, degraded)
@@ -121,16 +140,15 @@ def main():
         ssim_difference = ssim_predicted - ssim_degraded
         return psnr_degraded, psnr_predicted, psnr_difference, ssim_degraded, ssim_predicted, ssim_difference
 
+    
+    # MODEL 3 
     def train_model(image_size):
         inputs = Input(shape=(image_size, image_size))
         # Transpose the input shape replacing rows with columns and columns with rows
         inputs_t = tf.transpose(inputs, perm=[0, 2, 1])
-        # We perform this manually for ease of visualization, however can be done in a for loop
-        # MODEL 3
-        # Layer 1
         # Return sequences is TRUE as we want an output for every timestep, and not a "many-to-one" output
         # Merge_mode is set to AVERAGE - in order to maintain dimensionality (256,256) [default is CONCAT]
-        output_1 = (Bidirectional(GRU(image_size, return_sequences=True), merge_mode="ave"))(inputs_t)
+        output_1 = (Bidirectional(GRU(image_size, return_sequences=True), merge_mode="ave"))(inputs_t) 
         # The input of layer 2 is the output of layer 0 [transpose] SUBTRACT the output of layer 1
         input_2 = tf.keras.layers.Subtract()([inputs_t, output_1])
         output_2 = (Bidirectional(GRU(image_size, return_sequences=True), merge_mode="ave"))(input_2)
@@ -175,33 +193,30 @@ def main():
     print("Degrading Testing Data...")
     clean_testing_data, model_testing_data = degrade_test_data()
 
-    # Set the number of epochs to 100
-    # In this case, X is our input and z is our target - as opposed to the conventional X and y
     loss_train = []
     loss_val = []
     X_debug = []
-    z_debug = []
-
+    z_debug = []    
+    
     model = train_model(image_size)
     for i in range(num_epochs):
         X = []
         z = []
         X_validation = []
         z_validation = []
-
+        
         for images, label in training_data:
             noisy_image, _ = degrade(images, 25)
             # We append the training images to X and z
             X.append(noisy_image / 255.0)  # X is the Dirty Training Images
             z.append(images / 255.0)  # z is the Clean Training Images [Target]
-
+            
         for images, label in validation_data:
             noisy_image, _ = degrade(images, 25)
             X_validation.append(noisy_image / 255.0)  # X_validation is the Dirty Validation Images
             z_validation.append(images / 255.0)  # z_validation is the Clean Validation Images [Target]
-
+            
         print("Adding Noise Instances... Done")
-
         print("Reshaping Arrays..")
         X_debug = X[0:2]
         z_debug = z[0:2]
@@ -211,9 +226,8 @@ def main():
         z_validation = np.array(z_validation)
         X_debug = np.array(X_debug)
         z_debug = np.array(z_debug)
-
         print("Reshaping Arrays... Done")
-
+        
         # Calculate loss
         loss_metrics = model.fit(X, z,
                                  batch_size=100,
@@ -250,9 +264,13 @@ def main():
     plt.grid()
     plt.show()
 
-    # Test out network using the CLEAN TEST Images, as we defined at the top of the code
+    # Predict clean images using our degraded test images
     output_test = model.predict(model_testing_data)
-
+    
+    # Next, over all outputs of the test we wish to calculate the average of the PSNR and SSIM metrics 
+    # We append the respective PSNR and SSIM values to their lists, and calculate the average of each list
+    # We take the average of ALL test images: len(output_test) = len(testing_data)
+    # Display (print) these results on console
     psnr_degraded_lst = []
     psnr_predicted_lst = []
     psnr_difference_lst = []
@@ -260,7 +278,7 @@ def main():
     ssim_predicted_lst = []
     ssim_difference_lst = []
 
-    for i in range(2000):
+    for i in range(len(output_test)):
         psnr_degraded, psnr_predicted, psnr_difference, ssim_degraded, ssim_predicted, ssim_difference = \
             psnr_ssim_metrics(clean_testing_data[i], output_test[i], model_testing_data[i])
         psnr_degraded_lst.append(psnr_degraded)
@@ -271,16 +289,16 @@ def main():
         ssim_difference_lst.append(ssim_difference)
 
     psnr_degraded_average = average(psnr_degraded_lst)
-    print("The average PSNR of all Test Degraded Images wrt Ground Truth: " + str(psnr_degraded_average))
+    print("The average PSNR of all Test Degraded Images w.r.t Ground Truth: " + str(psnr_degraded_average))
     psnr_predicted_average = average(psnr_predicted_lst)
-    print("The average PSNR of all Test Predicted Images wrt Ground Truth: " + str(psnr_predicted_average))
+    print("The average PSNR of all Test Predicted Images w.r.t Ground Truth: " + str(psnr_predicted_average))
     psnr_difference_average = average(psnr_difference_lst)
     print("The average PSNR Difference: " + str(psnr_difference_average))
 
     ssim_degraded_average = average(ssim_degraded_lst)
-    print("The average SSIM of all Test Degraded Images wrt Ground Truth: " + str(ssim_degraded_average))
+    print("The average SSIM of all Test Degraded Images w.r.t Ground Truth: " + str(ssim_degraded_average))
     ssim_predicted_average = average(ssim_predicted_lst)
-    print("The average SSIM of all Test Predicted Images wrt Ground Truth: " + str(ssim_predicted_average))
+    print("The average SSIM of all Test Predicted Images w.r.t Ground Truth: " + str(ssim_predicted_average))
     ssim_difference_average = average(ssim_difference_lst)
     print("The average SSIM Difference: " + str(ssim_difference_average))
 
@@ -289,6 +307,9 @@ def main():
     test_image = create_subplots(model_testing_data[0], clean_testing_data[0], output_test[0], "test")
     test_image.show()
 
+    
+    # Save 15 sample test images (subplots consisting of ground truth, degraded, and predicted) and corresponding metrics
+    # These files are saved in test_output
     for i in range(15):
         test_image = create_subplots(model_testing_data[i], clean_testing_data[i], output_test[i], "test")
         test_image.savefig(test_output + str(i) + ".png")
@@ -305,12 +326,7 @@ def main():
                    + " \nSSIM Difference: " + str(ssim_difference))
         file.close()
 
-    # Comparing PSNR and SSIM
-    # Compare PSNR where Img_True = Clean Testing Data and Img_Test = Degraded Original Testing Data
-    # Then, compare PSNR2 where Img_True = Clean Image and Img_Test = Output of Network
-    # PSNR2 > PSNR. This indicates that the PSNR of the PREDICTED image is greater than the PSNR of the ORIGINAL image
-
-    # We can perform the same operation as above, but this time to one of the TRAINING images
+    # We can perform the same operation as above, but this time to one of the training images
     # Recall, X_debug is a subset of training images with random noise (we defined this in the network itself)
     output_train = model.predict(X_debug)
     print("Train Image Sample...")
